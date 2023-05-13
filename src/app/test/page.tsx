665665ab52bfbdb90f2baf1de89259312e5333c9
dohye <dohye0806@gmail.com>
2023-05-13 19:00:31 +0900

    second.tsx test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,7 @@
 
 import axios from 'axios';
 import Data from './data';
+import Second from './second';
 
 
 async function getData(): Promise<string> {
@@ -15,11 +16,13 @@ async function getData(): Promise<string> {
     },
   })
   .then((response) => {
-    console.log(response); // 서버로부터 받은 응답 데이터를 출력
-    return "success";
+    console.log("start")
+    console.log(response.data); // 서버로부터 받은 응답 데이터를 출력
+    console.log("end")
+    return "hello";
   })
   .catch((error) => {
-    console.error(error); // 에러 발생 시 에러를 출력
+    console.error(error); // 에러 ㄴ발생 시 에러를 출력
     return "fail";
   });
 
@@ -30,13 +33,14 @@ async function getData(): Promise<string> {
    
 export default async function Page(){
 
-
   const data_: string = await getData();
   console.log(data_);
+  console.log("!");
 
   return (
     <div>
       <Data response={data_}/>
+      <Second response={data_}/>
     </div>
   )
 }
