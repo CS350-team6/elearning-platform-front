9e2968e101b12a80b16d4732a4cb1142aa6dcc76
Sehyeon Kim <sue2368@gmail.com>
2023-05-13 20:28:47 +0900

    redux-persist test completed

--- src/app/test/data.tsx
+++ src/app/test/data.tsx
@@ -1,13 +1,28 @@
 "use client";
 
+import { incrementByAmount } from "@/redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 interface MyProps{
-  response : string;
+  response : number;
 }
+
+
+
 export default function Data({response}:MyProps) {
   console.log("??")
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
+  const apply = () => {
+    dispatch(incrementByAmount(response))
+  }
+
   return(
     <div>
-      {response}
+      <button onClick={apply}>apply</button>
+      <div>{response}</div>
+      <div>{count}</div>
+      
     </div>
   )
 }
