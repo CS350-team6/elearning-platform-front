45cfb494bd1da3c134e705c158e0fa489322de87
Sehyeon Kim <sue2368@gmail.com>
2023-05-13 20:28:47 +0900

    redux-persist test completed

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,7 @@ import Data from './data';
 import Second from './second';
 
 
-async function getData(): Promise<string> {
+async function getData(): Promise<number> {
 
   
   return axios.post('http://localhost:8000/users', {
@@ -19,11 +19,11 @@ async function getData(): Promise<string> {
     console.log("start")
     console.log(response.data); // 서버로부터 받은 응답 데이터를 출력
     console.log("end")
-    return "hello";
+    return 10;
   })
   .catch((error) => {
     console.error(error); // 에러 ㄴ발생 시 에러를 출력
-    return "fail";
+    return 1;
   });
 
    
@@ -33,14 +33,14 @@ async function getData(): Promise<string> {
    
 export default async function Page(){
 
-  const data_: string = await getData();
+  const data_: number = await getData();
   console.log(data_);
   console.log("!");
 
   return (
     <div>
       <Data response={data_}/>
-      <Second response={data_}/>
+      {/* <Second response={data_}/> */}
     </div>
   )
 }
