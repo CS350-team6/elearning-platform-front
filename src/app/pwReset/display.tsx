4c1c6130450b5e44d486e7581ec0207c73744a45
dohye <dohye0806@gmail.com>
2023-05-21 22:58:03 +0900

    import order

--- src/app/pwReset/display.tsx
+++ src/app/pwReset/display.tsx
@@ -2,23 +2,16 @@
 
 import { useRouter } from 'next/navigation';
 
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import React, {  useState } from 'react';
 const defaultTheme = createTheme();
-
-import React, { useEffect, useState } from 'react';
-
 
 interface MyProps{
   getData: (name: string) => Promise<boolean>;
