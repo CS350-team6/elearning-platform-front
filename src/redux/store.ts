54a63e73341c30d78d786cf1d233ac6d26a5ba3e
dohye <dohye0806@gmail.com>
2023-05-21 23:04:49 +0900

    Revert "Dohye new (#11)"
    This reverts commit 5672f87ac26677290f7450496881a6816df6ea29.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,8 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-import counterReducer from "./features/counterSlice";
-
-import { userApi } from "./services/userApi";
-
-=======
 import counterReducer from "./features/counterSlice";
 import { userApi } from "./services/userApi";
->>>>>>> main
-=======
-import counterReducer from "./features/counterSlice";
-import { userApi } from "./services/userApi";
->>>>>>> main
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 const persistConfig = {
@@ -26,18 +13,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
     counterReducer,
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
     [userApi.reducerPath]: userApi.reducer,
-
-=======
-    [userApi.reducerPath]: userApi.reducer,
->>>>>>> main
-=======
-    [userApi.reducerPath]: userApi.reducer,
->>>>>>> main
 })
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
@@ -47,15 +23,7 @@ export const store = configureStore({
     reducer: persistedReducer,
     devTools: process.env.NODE_ENV !== "production",
     middleware: (getDefaultMiddleware) =>
-<<<<<<< HEAD
-<<<<<<< HEAD
-        getDefaultMiddleware({serializableCheck: false}).concat([userApi.middleware]),
-=======
-        getDefaultMiddleware({}).concat([userApi.middleware]),
->>>>>>> main
-=======
         getDefaultMiddleware({ serializableCheck: false }).concat([userApi.middleware]),
->>>>>>> main
 });
 
 export const persistor = persistStore(store);
